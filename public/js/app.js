5053cdddfb144dd1c57cdc1ffd14ff72ea21570f
Luis Antonio Parrado <luisprmat@gmail.com>
2022-06-23 23:25:53 -0500

    Install Laravel Websockets to replace Pusher

--- public/js/app.js
+++ public/js/app.js
@@ -5324,9 +5324,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "974e51108f92334ee46b",
-  cluster: "mt1",
-  forceTLS: true
+  key: "974e51108f92334ee46c",
+  wsHost: window.location.hostname,
+  wsPort: 6001,
+  forceTLS: false,
+  disableStats: true
 });
 
 /***/ }),
